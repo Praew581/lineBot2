dd5364a8e95dbe92ba4a2cf1015c81a3d16ac876
Praew581 <51728357+Praew581@users.noreply.github.com>
2019-08-21 10:06:04 +0700

    Update app.py

--- app.py
+++ app.py
@@ -23,9 +23,9 @@ def callback():
     user = decoded["events"][0]['replyToken']
     userText = decoded["events"][0]['message']['text']
     #sendText(user,userText)
-    if  :(userText == 'สวัสดี') :
+    if (userText == 'สวัสดี') :
        sendText(user,'เคค่ะ')
-    elif:(userText == 'ไปละ') :
+    elif (userText == 'ไปละ') :
        sendText(user,'บาย')
     else:
        sendText(user,'ไม่เข้าใจ)
